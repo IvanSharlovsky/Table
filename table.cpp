0f5fbe6ade30bce6fb5a0ceab10321fb10a93c53
Ivan <ivansharlovsky@gmail.com>
2023-04-19 04:57:01 +0300

    Start table

---
+++ table.cpp
@@ -0,0 +1,54 @@
+#include <sqlite_modern_cpp.h>
+
+#include <iostream>
+
+void input(int &map_id,int &race_id int &time_score);
+void data_registration(int map_id, int race_id int time_score);
+void show_record(int map_id);
+
+int main(int, char**) 
+{
+    int map_id;
+    int race_id;
+    int time_score;
+
+    input(map_id, race_id, time_score);
+    data_registration(map_id, race_id, time_score);
+    input(map_id, race_id, time_score);
+    data_registration(map_id, race_id, time_score);
+    input(map_id, race_id, time_score);
+    data_registration(map_id, race_id, time_score);
+
+    show_record(map_id);
+}
+
+void input(int &map_id, int &race_id, int &time_score)
+{
+    std::cout << "Input map id: ";
+    std::cin >> map_id;
+    std::cout << "Input race id: ";
+    std::cin >> race_id;
+    std::cout << "Input time score: ";
+    std::cin >> time_score;
+}
+
+void data_registration(int map_id, int race_id, int time_score)
+{
+    sqlite::database db("table_records.db");
+
+    db << "create table if not exists records ("
+          "   _id integer primary key autoincrement not null,"
+          "   map_id int,"
+          "   race_id int"
+          "   time_score int"
+          ");"
+
+    db << "insert into records (map_id, race_id, time_score) values (?, ?, ?);" << map_id << race_id << time_score;
+}
+
+void show_record(int map_id);
+{
+    int best_time_score;
+    (db << "select max(time_score) from records where map_id = ?" << map_id) >> best_time_score;
+    std::cout << "The best result for map " << map_id << " is: " << best_time_score << "\n";
+}
