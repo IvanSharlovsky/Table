ae489a85680a7032b08b963d8aa8b3bb6be19e06
IvanSharlovsky <ivansharlovsky@gmail.com>
2023-04-19 19:30:56 +0300

    Working build

--- table.cpp
+++ table.cpp
@@ -2,24 +2,34 @@
 
 #include <iostream>
 
-void input(int &map_id,int &race_id int &time_score);
-void data_registration(int map_id, int race_id int time_score);
+void input(int &map_id, int &race_id, int &time_score);
+void data_registration(int map_id, int race_id, int time_score);
 void show_record(int map_id);
 
 int main(int, char**) 
 {
-    int map_id;
-    int race_id;
-    int time_score;
+    try 
+    {
+        int map_id;
+        int race_id;
+        int time_score;
 
-    input(map_id, race_id, time_score);
-    data_registration(map_id, race_id, time_score);
-    input(map_id, race_id, time_score);
-    data_registration(map_id, race_id, time_score);
-    input(map_id, race_id, time_score);
-    data_registration(map_id, race_id, time_score);
+        input(map_id, race_id, time_score);
+        data_registration(map_id, race_id, time_score);
+        input(map_id, race_id, time_score);
+        data_registration(map_id, race_id, time_score);
+        input(map_id, race_id, time_score);
+        data_registration(map_id, race_id, time_score);
 
-    show_record(map_id);
+        std::cout << "Which map id: ";
+        std::cin >> map_id;
+
+        show_record(map_id);
+    }
+    catch (std::exception& e) 
+    {
+      std::cout << e.what() << "\n";
+    }
 }
 
 void input(int &map_id, int &race_id, int &time_score)
@@ -39,15 +49,17 @@ void data_registration(int map_id, int race_id, int time_score)
     db << "create table if not exists records ("
           "   _id integer primary key autoincrement not null,"
           "   map_id int,"
-          "   race_id int"
+          "   race_id int,"
           "   time_score int"
-          ");"
+          ");";
 
     db << "insert into records (map_id, race_id, time_score) values (?, ?, ?);" << map_id << race_id << time_score;
 }
 
-void show_record(int map_id);
+void show_record(int map_id)
 {
+    sqlite::database db("table_records.db");
+
     int best_time_score;
     (db << "select max(time_score) from records where map_id = ?" << map_id) >> best_time_score;
     std::cout << "The best result for map " << map_id << " is: " << best_time_score << "\n";
